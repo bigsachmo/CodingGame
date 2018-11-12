6ba5ebe385631da544b1525c35ac433bc634c6f9
Stan <stan.hubert@free.fr>
2018-11-12 23:01:12 +0100

    Fix Program

--- AsciiGame/Program.cs
+++ AsciiGame/Program.cs
@@ -7,6 +7,22 @@ namespace AsciiGame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string E = @"
+### 
+#   
+##  
+#   
+### 
+            ";
+
+            string B = @"
+##  
+# # 
+##  
+# # 
+##        
+            ";
+            Console.Write (E + " " +B);
         }
     }
 }
