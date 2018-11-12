d9dfde6cf1fd5920e5e45371ed1870297c51bd5a
Stan <stan.hubert@free.fr>
2018-11-12 21:07:22 +0100

    Init Ascci Game

---
+++ AsciiGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsciiGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
