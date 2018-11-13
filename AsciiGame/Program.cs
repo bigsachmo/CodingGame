be1c49b9f9f6dffed3f32827da475a14e42d10e4
Stan <stan.hubert@free.fr>
2018-11-13 23:46:12 +0100

    Asscii Contest OK

--- AsciiGame/Program.cs
+++ AsciiGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace AsciiGame
 {
@@ -9,23 +10,51 @@ namespace AsciiGame
 
             // int L = int.Parse(Console.ReadLine());
             // int H = int.Parse(Console.ReadLine());
-            string T = Console.ReadLine();
+            //string T = Console.ReadLine();
             // for (int i = 0; i < H; i++)
             // {
             //     string ROW = Console.ReadLine();
             // }
-            string[] alphabet = new string[]{"A","B","C","D", "E","F","G","H","I","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-            Console.WriteLine(Array.IndexOf(alphabet,"1"));
 
-            for (int j =0; j < T.Length; j++)
+            int L = 4;
+            int H = 5;
+            string T = "M@nHattan";
+            string[] rows = new string[H];
+            rows[0] = " #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ";
+            rows[1] = "# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # ";
+            rows[2] = "### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## ";
+            rows[3] = "# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       ";
+            rows[4] = "# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  ";
+
+            string[] alphabet = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            int[] letterIndexes = new int[T.Length];
+
+            for (int i = 0; i < T.Length; i++)
             {
-                Console.WriteLine(T[j]);
+                int letterIndex = Array.IndexOf(alphabet, T[i].ToString().ToUpper());
+                if (letterIndex == -1)
+                {
+                    letterIndex = 26;
+                }
+                letterIndexes[i] = letterIndex;
             }
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
 
-            Console.WriteLine("answer");
+            StringBuilder word = new StringBuilder();
+            for (int line = 0; line < H; line++)
+            {
+                StringBuilder lineLetter = new StringBuilder();
+                foreach (int letterIndex in letterIndexes)
+                {
+                    string letter = rows[line].Substring(letterIndex * L, L);
+                    lineLetter.Append(letter);
+                }
+                // Console.WriteLine(letter);
+                word.AppendLine(lineLetter.ToString());
+            }
+            // Console.WriteLine();
+            Console.Write(word.ToString());
+
         }
     }
 }
