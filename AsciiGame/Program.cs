488e7d555c86bfeb12bf37503d357de1eb6d8acb
Stanislas Hubert <stanislas.hubert@sogeti.com>
2018-11-13 14:47:04 +0100

    Test Ascii

--- AsciiGame/Program.cs
+++ AsciiGame/Program.cs
@@ -6,15 +6,26 @@ namespace AsciiGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string E = @"
-### 
-#   
-##  
-#   
-### 
-            ";
-            Console.Write (E);
+
+            // int L = int.Parse(Console.ReadLine());
+            // int H = int.Parse(Console.ReadLine());
+            string T = Console.ReadLine();
+            // for (int i = 0; i < H; i++)
+            // {
+            //     string ROW = Console.ReadLine();
+            // }
+            string[] alphabet = new string[]{"A","B","C","D", "E","F","G","H","I","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+            Console.WriteLine(Array.IndexOf(alphabet,"1"));
+
+            for (int j =0; j < T.Length; j++)
+            {
+                Console.WriteLine(T[j]);
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine("answer");
         }
     }
 }
