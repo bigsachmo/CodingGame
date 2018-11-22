963cf22550f064e969ee4b46dbd1988813ad38ca
Stan <stan.hubert@free.fr>
2018-11-22 17:09:10 +0100

    Init Shadows Knight

---
+++ ShadowsKnight/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShadowsKnight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shadows Knight Coding Game!");
+        }
+    }
+}
