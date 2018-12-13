98c759ea273c26d65166fcace4fa207fbf2ea42d
Stan <stan.hubert@free.fr>
2018-12-13 16:34:07 +0100

    Mimtype

--- MimeType/Program.cs
+++ MimeType/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace MimeType
 {
@@ -6,7 +11,44 @@ namespace MimeType
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mime Type");
+            int N = int.Parse(Console.ReadLine()); // Number of elements which make up the association table.
+            int Q = int.Parse(Console.ReadLine()); // Number Q of file names to be analyzed.
+
+            Dictionary<string, string> mimeTypes = new Dictionary<string, string>();
+            HashSet<string> files = new HashSet<string>();
+            for (int i = 0; i < N; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                string EXT = inputs[0]; // file extension
+                string MT = inputs[1]; // MIME type.
+                mimeTypes[EXT.ToLower()] = MT;
+            }
+            for (int i = 0; i < Q; i++)
+            {
+                string FNAME = Console.ReadLine(); // One file name per line.
+                files.Add(FNAME.ToLower());
+            }
+
+            foreach (string file in files)
+            {
+                string answer = "UNKNOWN";
+                int lastPoint = file.LastIndexOf('.');
+                if (lastPoint == -1)
+                {
+                    Console.WriteLine(answer);
+                    continue;
+                }
+
+                if (lastPoint + 1 < file.Length)
+                {
+                    string extension = file.Substring(lastPoint + 1).ToLower();
+                    if (mimeTypes.ContainsKey(extension.ToLower()))
+                    {
+                        answer = mimeTypes[extension];
+                    }
+                }
+                Console.WriteLine(answer);
+            }
         }
     }
 }
