4ddf23c81a2cb7f2c56289cecd214632def8355d
radiohub <stan.hubert@free.fr>
2019-05-02 22:28:38 +0200

    init rugby scroe

---
+++ CodingGame.Test/RugbyScoreTest.cs
@@ -0,0 +1,18 @@
+using System;
+using NUnit.Framework;
+
+
+
+namespace RugbyScoreUT
+{
+    [TestFixture]
+    public class RugbyScoreTest
+    {
+       [Test]
+       public void Test12() 
+       {
+            Console.WriteLine("Display test");
+            Assert.IsTrue(true);
+       }
+    }
+}
---
+++ RugbyScore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RugbyScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string answer = ComputeScore(N);
+            Console.WriteLine(answer);
+        }
+
+        public static string ComputeScore (int score)
+        {
+            string result = "";
+            return result;
+        }
+    }
+}
