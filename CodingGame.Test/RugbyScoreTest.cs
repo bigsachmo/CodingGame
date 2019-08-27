5ed955923e92cd5cf1e3e4c1af7fb54fd223cf60
Stan Hubert <stan.hubert@free.fr>
2019-08-27 17:32:12 +0200

    Complete Rugby Score

--- CodingGame.Test/RugbyScoreTest.cs
+++ CodingGame.Test/RugbyScoreTest.cs
@@ -13,8 +13,24 @@ namespace RugbyScoreUT
         public void Test12()
         {
             string result = ComputeRugbyScore.ComputeScore(12);
-            string expectedResult = @"0 0 4" + System.Environment.NewLine + "2 1 0";
-            Assert.AreEqual(result, expectedResult);
+            string expectedResult = "0 0 4\r\n2 1 0\r\n";
+            Assert.AreEqual(expectedResult , result );
        }
+
+        [Test]
+        public void Test15()
+        {
+            string result = ComputeRugbyScore.ComputeScore(15);
+            string expectedResult = "0 0 5\r\n2 1 1\r\n3 0 0\r\n";
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void Test21()
+        {
+            string result = ComputeRugbyScore.ComputeScore(21);
+            string expectedResult = "0 0 7\r\n2 1 3\r\n3 0 2\r\n3 3 0\r\n";
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
--- RugbyScore/Program.cs
+++ RugbyScore/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace RugbyScore
 {
@@ -8,7 +13,43 @@ namespace RugbyScore
         {
             int N = int.Parse(Console.ReadLine());
             string answer = ComputeRugbyScore.ComputeScore(N);
-            Console.WriteLine(answer);
+            
+        }
+    }
+
+    public class ComputeRugbyScore
+    {
+        public static string ComputeScore(int score)
+        {
+            StringBuilder results = new StringBuilder();
+            int tries = 0;
+            int pens = 0;
+            while ( tries * 5 <= score)
+            {
+                //test each solution with that number of tries ==> first only with penalties or drops
+                for (int icons = 0; icons <= tries; icons++)
+                {
+                    int newScore = currentScore(tries, icons, pens);
+                    while (newScore <= score)
+                    {
+                        if (newScore == score)
+                        {
+                            Console.WriteLine(string.Format("{0} {1} {2}", tries, icons, pens));
+                            results.AppendLine(string.Format("{0} {1} {2}", tries, icons, pens));
+                        }
+                        pens++;
+                        newScore = currentScore(tries, icons, pens);
+                    }
+                    pens = 0;
+                }
+                tries++;
+            }
+            return results.ToString();
+        }
+
+        private static int currentScore(int simpleTry, int conversion, int pen)
+        {
+            return simpleTry * 5 + conversion * 2 + pen * 3;
         }
     }
 }
